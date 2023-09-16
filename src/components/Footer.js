15277b72aa0d53ef14b87fdcf4eff61c03a561bf
Tapasya <tapasya1011@gmail.com>
2023-09-16 23:29:15 +0530

    updating footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import '../css/Footer.css'
-
+import React from "react";
+import "../css/Footer.css";
 
 function Footer() {
-  return (
-    <div className=" text-dark mt-5 footer bg-primary py-3" >
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-2  text-center">
-            <img src="assets/images/logo/TY-logo/1.png" alt="logo" className="footer__logo" />       
-          </div>
-          
-          </div>
-        </div>
-    </div>
-    
-  )
+	return (
+		<div className=" text-dark mt-5 footer bg-primary py-3">
+			<div className="container">
+				<div className="row text-center">
+					<div className="">© Prerna Saxena</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Footer
+export default Footer;
