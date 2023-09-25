0e3192949d04de7c1250dfd2eb92687699f33e13
Tapasya <tapasya1011@gmail.com>
2023-09-25 15:23:25 +0530

    changing color of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import "../css/Footer.css";
 
 function Footer() {
 	return (
-		<div className=" text-dark mt-5 footer bg-primary py-3">
+		<div className=" text-dark mt-5 footer  py-3">
 			<div className="container">
 				<div className="row text-center">
 					<div className="">© Prerna Saxena</div>
